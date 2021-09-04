7605fadbf3a2aa6d74d9925171f26eb382d06f40
RizkiTea <89777565+RizkiTea@users.noreply.github.com>
2021-09-05 05:36:24 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,6 +1,6 @@
 #monero
-sudo apt update
+#sudo apt update
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.6.7/SRBMiner-Multi-0-6-7-Linux.tar.xz
 tar xf SRBMiner-Multi-0-6-7-Linux.tar.xz 
 cd SRBMiner-Multi-0-6-7
-./SRBMiner-MULTI --algorithm randomx --pool us-west.minexmr.com:4444 --wallet 83CvYRyGBVudKUhfnBQ1uLDw9nVZWNpdNienTS9nqfmDQrKZ17G9gzgBuTW8MLYBMaQgb5UxMfGmXFFJWUQw3ohTL58E2Uv+37500.ibenk01 --password x --cpu-threads 4 
+./SRBMiner-MULTI --algorithm randomx --pool us-west.minexmr.com:4444 --wallet 83CvYRyGBVudKUhfnBQ1uLDw9nVZWNpdNienTS9nqfmDQrKZ17G9gzgBuTW8MLYBMaQgb5UxMfGmXFFJWUQw3ohTL58E2Uv+37500.ibenk01 --password x --cpu-threads 2 
